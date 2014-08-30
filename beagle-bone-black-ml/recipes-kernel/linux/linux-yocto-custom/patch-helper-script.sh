c23029ccb81f7ee1db5d1a2f24227bf790aa515e
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2014-08-30 17:00:53 +0300

    3.8.13 kernel

--- beagle-bone-black-ml/recipes-kernel/linux/linux-yocto-custom/patch-helper-script.sh
+++ beagle-bone-black-ml/recipes-kernel/linux/linux-yocto-custom/patch-helper-script.sh
@@ -6,7 +6,8 @@ export ECHO="$(which echo)"
 PATCHPATH="patches"
 
 # It is essential to apply the patches in the right order !
-PATCHSET="dts fixes usb dts-bone dts-bone-capes static-capes saucy"
+PATCHSET="dma rtc pinctrl cpufreq adc i2c da8xx-fb pwm mmc crypto 6lowpan capebus arm omap omap_sakoman omap_beagle_expansion omap_beagle omap_panda net drm not-capebus pru usb PG2 reboot iio w1 gpmc mxt ssd130x build hdmi audio resetctrl camera resources pmic pps leds capes proto logibone fixes firmware saucy machinekit sgx backports"
+# xenomai"
 
 OUTPUT_FILE="proposed_beagle-bone-black-ml-user-patches.scc"
 
